64c2cf32b49a7254de49a37bf6b123ad2b618aee
caroem <chi340@hotmail.com>
2016-05-09 21:22:45 +0200

    Fixed FileUploader error SAVI-84

--- complete/src/main/java/sapaca/Application.java
+++ complete/src/main/java/sapaca/Application.java
@@ -5,9 +5,15 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.apache.http.util.EntityUtils;
+import org.bytedeco.javacpp.opencv_core.IplImage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpEntity;
+
+import java.io.File;
+import java.net.URL;
+
+import static org.bytedeco.javacpp.opencv_highgui.cvLoadImage;
 
 @SpringBootApplication
 public class Application {
@@ -15,7 +21,16 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 
-		// ImageUploader imageUploader = new ImageUploader("pathToFile");
-		ImageUploader imageUploader = new ImageUploader();
+
+		try {
+			// Beispiel wie es aussehen koennte...
+			URL url = Application.class.getClassLoader().getResource("TestImages/obama.jpg");
+			IplImage imageToBeUploaded = cvLoadImage(new File(url.toURI()).getAbsolutePath());
+
+			ImageUploader imageUploader = new ImageUploader(imageToBeUploaded);
+		}
+		catch (Exception e) {
+			System.out.println("Fuck u.");
+		}
 	}
 }
--- complete/src/main/java/sapaca/ImageUploader.java
+++ complete/src/main/java/sapaca/ImageUploader.java
@@ -1,9 +1,14 @@
 package sapaca;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.bytedeco.javacpp.opencv_core.IplImage;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.Map;
 
 /**
@@ -11,21 +16,40 @@ import java.util.Map;
  */
 public class ImageUploader {
     private Cloudinary cloudinary;
+    private IplImage imageToBeUploaded;
+    private BufferedImage buffImage;
+    private File outputFile;
 
-    //public ImageUploader(String filePath) {
-    public ImageUploader() {
+    public ImageUploader(IplImage imageToBeUploaded) {
+        this.imageToBeUploaded = imageToBeUploaded;
+        setCloudinary();
+        uploadImage(IplImageToFile());
+    }
 
+    private void setCloudinary() {
         cloudinary = new Cloudinary(ObjectUtils.asMap(
                 "cloud_name", "drrgdjljd",
                 "api_key", "259618711299969",
                 "api_secret", "y7T9lxX6GURHldFASQzbclbbutg"));
+    }
 
-        // File file = new File(filePath);
-        File file = new File("C:\\Users\\caro\\IdeaProjects\\FaceRecognition\\complete\\src\\main\\resources\\TestImages\\kurtcobain.jpg");
-        uploadImage(file);
+    private File IplImageToFile() {
+        buffImage = imageToBeUploaded.getBufferedImage();
+        outputFile = new File("image.jpg");
+        try {
+            ImageIO.write(buffImage, "jpg", outputFile);
+            return outputFile;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private String uploadImage(File file) {
+        if (file == null) {
+            return "";
+        }
+
         String uploadedUrl;
         try {
             Map uploadResult = cloudinary.uploader().upload(file, ObjectUtils.asMap("public_id", "url"));
@@ -36,5 +60,25 @@ public class ImageUploader {
             e.printStackTrace();
         }
         return uploadedUrl;
+    }
+
+    private String getURL(String nameOfFile) {
+        try {
+            URL url = ImageUploader.class.getClassLoader().getResource(nameOfFile);
+            System.out.println(new File(url.toURI()).getAbsolutePath());
+            return new File(url.toURI()).getAbsolutePath();
+        }
+        catch (URISyntaxException e) {
+            System.out.println("Source not found.");
+            return "";
+        }
+        catch (Exception e) {
+            System.out.println("Something went horribly wrong...");
+            return " ";
+        }
+    }
+
+    public Cloudinary getCloudinary() {
+        return cloudinary;
     }
 }
