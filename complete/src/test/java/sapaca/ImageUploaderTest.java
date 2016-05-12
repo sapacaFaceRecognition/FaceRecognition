e2b746058364576cd9b47b3031bac3efadb6531f
caroem <chi340@hotmail.com>
2016-05-12 21:59:48 +0200

    Debug11

--- complete/src/test/java/sapaca/ImageUploaderTest.java
+++ complete/src/test/java/sapaca/ImageUploaderTest.java
@@ -7,6 +7,9 @@ import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+import static org.bytedeco.javacpp.opencv_core.IPL_DEPTH_8U;
+import static org.bytedeco.javacpp.opencv_core.cvCreateImage;
+import static org.bytedeco.javacpp.opencv_core.cvSize;
 import static org.bytedeco.javacpp.opencv_highgui.cvLoadImage;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -16,20 +19,15 @@ import static org.junit.Assert.assertThat;
 public class ImageUploaderTest {
     private ImageUploader imageUploader;
     private IplImage imageToBeUploaded;
-//    @Before
-//    public void setUp() {
-//        try {
-//            URL url = ImageUploaderTest.class.getClassLoader().getResource("TestImages/cwurst.jpg");
-//            imageToBeUploaded = cvLoadImage(new File(url.toURI()).getAbsolutePath());
-//        } catch (URISyntaxException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//    @Test
-//    public void imageTest() throws Exception {
-//        imageUploader = new ImageUploader(imageToBeUploaded);
-//        assertThat(imageToBeUploaded, is(imageUploader.getImage()));
-//    }
+    @Before
+    public void setUp() {
+        imageToBeUploaded = cvCreateImage(cvSize(210, 210), IPL_DEPTH_8U, 1);
+
+    }
+
+    @Test
+    public void imageTest() throws Exception {
+        imageUploader = new ImageUploader(imageToBeUploaded);
+        assertThat(imageToBeUploaded, is(imageUploader.getImage()));
+    }
 }
