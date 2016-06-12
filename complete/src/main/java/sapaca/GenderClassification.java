fae3518183ccbc0333a032d0f099c2faf3e376a2
caroem <chi340@hotmail.com>
2016-06-12 23:16:50 +0200

    Used RFC metric; deleted unused methods; replaced small methods with if statements

--- complete/src/main/java/sapaca/GenderClassification.java
+++ complete/src/main/java/sapaca/GenderClassification.java
@@ -6,20 +6,9 @@ import com.mashape.unirest.http.exceptions.UnirestException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import static org.bytedeco.javacpp.opencv_core.IplImage;
-import static org.bytedeco.javacpp.opencv_core.Mat;
-import static org.bytedeco.javacpp.opencv_core.MatVector;
-import static org.bytedeco.javacpp.opencv_highgui.imread;
-import static org.bytedeco.javacpp.opencv_highgui.imwrite;
-import static org.bytedeco.javacpp.opencv_stitching.Stitcher;
-
-
 
 public class GenderClassification {
-	private static boolean tryUseGpu = false;
-	private static MatVector imgs = new MatVector();
-	private static String resultName = "result.jpg";
 	private static Gender gender;
-	private static int stitcherVar;
 	private IplImage image;
 	private String urlWithoutSlashes;
 	private HttpResponse<JsonNode> response;
@@ -31,6 +20,12 @@ public class GenderClassification {
 
 	public GenderClassification(IplImage image) {
 		this.image = image;
+		genderConfidence = 0.0;
+		gender = Gender.UNKNOWN;
+		raceConfidence = 0.0;
+		race = "";
+		age = 0;
+		ageRange = 0;
 		ImageUploader imageUploader = new ImageUploader(image);
 		String url = imageUploader.getUploadedUrl();
 		httpRequest(url);
@@ -38,24 +33,6 @@ public class GenderClassification {
 
 	public GenderClassification() {
 
-	}
-
-	private static Gender classifyGender(int retval) {
-		if (retval != 0) {
-			System.out.println("Something went wrong...");
-		}
-
-		Mat pano = new Mat();
-		Stitcher stitcher = Stitcher.createDefault(tryUseGpu);
-
-		int status = stitcher.stitch(imgs, pano);
-
-		if (status != Stitcher.OK) {
-			System.out.println("Something went wrong...");
-		}
-		imwrite(resultName, pano);
-		stitcherVar++;
-		return gender;
 	}
 
 	private static void printUsage() {
@@ -67,26 +44,6 @@ public class GenderClassification {
 						+ "      The default is 'result.jpg'.");
 	}
 
-	private static void stitcherArgs() {
-		String[] args = new String[2];
-		int usageLeft = 0;
-		if (args.length == 0) {
-			printUsage();
-			usageLeft =  -1;
-		}
-		for (int i = 2; i < args.length; i++) {
-			if ("--help".equals(args[i])) {
-				printUsage();
-				usageLeft = 1;
-			} else if ("--output".equals(args[i])) {
-				resultName = args[i + 1];
-				i++;
-			} else {
-				Mat img = imread(args[i]);
-			}
-		}
-	}
-
 	private String replaceSlashesInUrl(String url) {
 		if(url.length() > 1) {
 			String temp = url.replace(":", "%3A");
@@ -95,6 +52,7 @@ public class GenderClassification {
 		}
 		return "";
 	}
+
 	private void httpRequest(String uploadedImageUrl) {
 		urlWithoutSlashes = replaceSlashesInUrl(uploadedImageUrl);
 		if(urlWithoutSlashes.length() > 1) {
@@ -105,34 +63,20 @@ public class GenderClassification {
 						.header("X-Mashape-Key", "irY8JsoGe8msh97R53VBAlqi8FzRp10mJcrjsnvsM6bHNNcIVX")
 						.header("Accept", "application/json")
 						.asJson();
-				if(verifyResults()) {
+
+				String json = response.getBody().toString();
+
+				if ((json.contains("Male") || json.contains("Female"))) {
 					jsonGetGender();
 					jsonGetRace();
 					jsonGetAge();
-				} else{
-					setAttributesForFalseClassification();
 				}
 			} catch (UnirestException e) {
 				e.printStackTrace();
 			}
 		}
 	}
-	private boolean verifyResults() {
-		String json = response.getBody().toString();
-		if (json.contains("Male") || json.contains("Female")) {
-			return true;
-		}
-		return false;
-	}
 
-	private void setAttributesForFalseClassification() {
-		genderConfidence = 0.0;
-		gender = Gender.UNKNOWN;
-		raceConfidence = 0.0;
-		race = "";
-		age = 0;
-		ageRange = 0;
-	}
 	private void jsonGetGender() {
 		String json = response.getBody().toString();
 		String matcher = "";
@@ -187,31 +131,31 @@ public class GenderClassification {
 		age = Integer.parseInt(temp2);
 	}
 
-	public IplImage getImage() {
+	IplImage getImage() {
 		return image;
 	}
 
-	public String getUrlWithoutSlashes() {
+	String getUrlWithoutSlashes() {
 		return urlWithoutSlashes;
 	}
 
-	public HttpResponse<JsonNode> getResponse() {
+	HttpResponse<JsonNode> getResponse() {
 		return response;
 	}
 
-	public double getGenderConfidence() {
+	double getGenderConfidence() {
 		return genderConfidence;
 	}
 
-	public Gender getGender() {
+	Gender getGender() {
 		return gender;
 	}
 
-	public double getRaceConfidence() { return raceConfidence; }
+	double getRaceConfidence() { return raceConfidence; }
 
-	public String getRace() { return race; }
+	String getRace() { return race; }
 
-	public int getAge() { return age; }
+	int getAge() { return age; }
 
-	public int getAgeRange() { return ageRange; }
+	int getAgeRange() { return ageRange; }
 }
